a64198339c8ac670c1883d1c8a53d7e5f994feee
João Pedro Alves <jotalmeida007@hotmail.com>
2021-02-27 12:08:20 -0300

    Day 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { ExperienceBar } from './components/ExperienceBar';
+import './styles/global.css';
+
 function App() {
-  return <h1>Hello nlw</h1>;
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
 }
 
 export default App;
