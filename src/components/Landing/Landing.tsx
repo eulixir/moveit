76566b22045fc0d5be5e58faedd04ddea4e5766a
João Pedro Alves <jotalmeida007@hotmail.com>
2021-03-16 19:38:56 -0300

    Update landing container

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -7,54 +7,64 @@ import { DiReact } from 'react-icons/di';
 
 import { signIn, useSession } from 'next-auth/client';
 
+import { SwapSpinner } from 'react-spinners-kit';
+
 export function Landing() {
   const [session, loading] = useSession();
   const router = useRouter();
   return (
-    <div className={styles.Container}>
-      <section>
-        <div className={styles.logoContainer}>
-          <img src="/icons/Simbolo.png" alt="" />
-        </div>
-        <div className={styles.contentLoginContainer}>
-          <div className={styles.moveitContainer}>
-            <img src="/icons/logo.svg" alt="" />
-          </div>
-          <div className={styles.welcomeContainer}>
-            <strong>Bem-vindo</strong>
-          </div>
-          <div className={styles.githubContainer}>
-            <DiReact size={50} />
-            Faça login com sua conta github ou google para começar
-          </div>
-          <div className={styles.loginContainer}>
-            {!session && (
-              <>
-                <button
-                  type="submit"
-                  onClick={(): Promise<void> => signIn('github')}
-                >
-                  <ImGithub size={32} />
-                  Github
-                </button>
+    <>
+      {!session && (
+        <>
+          <div className={styles.Container}>
+            <section>
+              <div className={styles.logoContainer}>
+                <img src="/icons/Simbolo.png" alt="" />
+              </div>
+              <div className={styles.contentLoginContainer}>
+                <div className={styles.moveitContainer}>
+                  <img src="/icons/logo.svg" alt="" />
+                </div>
+                <div className={styles.welcomeContainer}>
+                  <strong>Bem-vindo</strong>
+                </div>
+                <div className={styles.githubContainer}>
+                  <DiReact size={50} />
+                  Faça login com sua conta github ou google para começar
+                </div>
+                <div className={styles.loginContainer}>
+                  <button
+                    type="submit"
+                    onClick={(): Promise<void> => signIn('github')}
+                  >
+                    <ImGithub size={32} />
+                    Github
+                  </button>
 
-                <button
-                  type="submit"
-                  onClick={(): Promise<void> => signIn('google')}
-                >
-                  <ImGoogle size={32} />
-                  Google
-                </button>
-              </>
-            )}
-            {session && (
-              <button type="submit" onLoad={() => router.push('/pomodoro')}>
-                <img src="/icons/login.svg" alt="" />
-              </button>
-            )}
+                  <button
+                    type="submit"
+                    onClick={(): Promise<void> => signIn('google')}
+                  >
+                    <ImGoogle size={32} />
+                    Google
+                  </button>
+                </div>
+              </div>
+            </section>
+          </div>
+        </>
+      )}
+      {session && (
+        <div className={styles.LoadingContainer}>
+          <div>
+            <img
+              src="/icons/login.svg"
+              onLoad={() => router.push('/pomodoro')}
+            />
+            <SwapSpinner size={200} color={'white'} />
           </div>
         </div>
-      </section>
-    </div>
+      )}
+    </>
   );
 }
